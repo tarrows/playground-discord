0b83134baad52ea7f4f13800a7a12464c16ac414
c10t <c10t@fa.ke>
2020-06-06 19:44:08 +0900

    hello-world

---
+++ quick_start.py
@@ -0,0 +1,26 @@
+import os
+import discord
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(API_KEY)
